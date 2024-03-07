ecfc4bcad19dbcfacc394c73f59a374686229fa9
shreyaskar-1 <shreyaskar_219031@gmail.com>
2024-03-07 13:03:20 +0530

    hero section

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,7 +2,19 @@
 
 function Hero() {
   return (
-    <div>Hero</div>
+    <div className="flex items-center justify-center w-screen h-screen">
+    <div className="w-[1292px] h-[556px] bg-yellow-400 justify-evenly flex flex-row">
+      <div className="w-[422px] h-[556px] flex flex-col bg-black">
+      
+      </div>
+      <div className="w-[422px] h-[556px] flex flex-col bg-black">
+      
+      </div>
+      <div className="w-[422px] h-[556px] flex flex-col bg-black">
+     
+      </div>
+    </div>
+  </div>
   )
 }
 
